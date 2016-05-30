de43dfb32b5a970a124a48947a21e7f66d08316e
Kevin Donahue <kevindonahuedevelopment@gmail.com>
2016-05-29 20:53:05 -0400

    begin api refactor

--- index.js
+++ index.js
@@ -1,49 +1,32 @@
 'use strict';
+const _ = require('lodash');
 const isPrimitive = require('is-primitive');
 
-module.exports = value => {
+const gate = (v, fn) => isPrimitive(v) ? v === null ? null + '' : typeof value : fn(v);
 
-  if (isPrimitive(value)) {
-    return { structure: [value === null ? null + '' : typeof value] };
-  }
+module.exports = value => gate(value, mapTypeStructure);
 
-  let endOfObj = null;
-  const mapTypeStructure = (obj, context, depth) => {
+module.exports.atDepth = (value, n) => {
+  return gate(value, v => {
+    return mapTypeStructure(v)
+      .filter(o => o.depth === n)
+      .map(o => o.type)
+  })
+}
 
-    const currentKeys = Object.keys(obj);
+function mapTypeStructure(obj, memo, depth, isEnd) {
+  const nonObj = v => isPrimitive(v) || typeof v === 'function';
 
-    context = context || [];
-    depth = depth || 0;
+  memo = memo || [];
+  depth = depth || 0;
 
-    for (const k in obj) {
-      if (obj.hasOwnProperty(k)) {
+  _.forEach(obj, v => {
+    const type = v === null ? null + '' : typeof v;
 
-        if (endOfObj && depth > 0) depth--;
-        if (!isPrimitive(obj[k]) && !Object.keys(obj[k]).length) continue;
-        if (!isPrimitive(obj[k]) && typeof obj[k] !== 'function') {
+    // if (isEnd && depth > 0) depth--;
+    if (isPrimitive(v)) memo.push({ type, depth });
+    else if (!nonObj(v)) mapTypeStructure(v, memo, ++depth, isEnd);
+  })
 
-          endOfObj = false;
-          mapTypeStructure(obj[k], context, ++depth);
-        } else if (isPrimitive(obj[k])) {
-
-          const type = obj[k] === null ? null : typeof obj[k];
-          endOfObj = k === currentKeys[currentKeys.length - 1];
-          context.push({
-            type: type + '',
-            depth: depth
-          })
-        }
-      }
-    }
-
-    return { context }
-  }
-
-  const atDepth = n =>  {
-    return mapTypeStructure(value).context
-      .filter(obj => obj.depth === n)
-      .map(obj => obj.type)
-  }
-
-  return { structure: mapTypeStructure(value).context, atDepth }
+  return memo;
 }
--- test.js
+++ test.js
@@ -1,35 +1,46 @@
 import test from 'ava';
+import map from './map';
 import fn from './';
-import map from './fixtures/map.js';
 
-test('.structure', t => {
-  t.deepEqual(fn(map.o.in).structure, map.o.out);
-  t.deepEqual(fn(map.a.in).structure, map.a.out);
+const values = [true, 42, 'x', undefined, null];
+const primitives = values.map(v => v === null ? null + '' : typeof v);
+
+test.failing('fn maps correct type and depth structure', t => {
+  // console.log(fn(map.o.in));
+  // console.log('\n\n');
+  // console.log(map.o.out);
+
+  t.deepEqual(fn(map.o.in), map.o.out);
+  t.deepEqual(fn(map.a.in), map.a.out);
 })
 
-test('.structure returns types for flat inputs', t => {
-  t.deepEqual(fn(42).structure, ['number']);
-  t.deepEqual(fn('x').structure, ['string']);
-  t.deepEqual(fn(true).structure, ['boolean']);
-  t.deepEqual(fn(null).structure, ['null']);
-  t.deepEqual(fn(undefined).structure, ['undefined']);
-  t.deepEqual(fn((n) => Math.sqrt(n)).structure, []);
-  t.deepEqual(fn({}).structure, []);
+test.failing('fn returns correct type on flat input', t => {
+  primitives.forEach((type, i) => t.deepEqual(fn(values[i]), type));
 })
 
-test('.atDepth() lists types at given depth', t => {
-  t.deepEqual(fn(map.o.in).atDepth(0), ['number', 'number']);
-  t.deepEqual(fn(map.o.in).atDepth(2), ['null']);
-  t.deepEqual(fn(map.o.in).atDepth(4), ['string']);
-  t.deepEqual(fn(map.o.in).atDepth(5), []);
-  t.deepEqual(fn(map.o.in).atDepth(1.2), []);
-  t.deepEqual(fn(map.o.in).atDepth(-2), []);
-  t.deepEqual(fn(map.o.in).atDepth(+Infinity), []);
-  t.deepEqual(fn(map.o.in).atDepth(-Infinity), []);
+// test.skip('fn returns types for flat inputs', t => {
+//   t.deepEqual(fn(42), ['number']);
+//   t.deepEqual(fn('x'), ['string']);
+//   t.deepEqual(fn(true), ['boolean']);
+//   t.deepEqual(fn(null), ['null']);
+//   t.deepEqual(fn(undefined), ['undefined']);
+//   t.deepEqual(fn((n) => Math.sqrt(n)), []);
+//   t.deepEqual(fn({}), []);
+// })
 
-  t.deepEqual(fn(map.a.in).atDepth(0), ['number', 'undefined']);
-  t.deepEqual(fn(map.a.in).atDepth(1), ['number', 'string']);
-  t.deepEqual(fn(map.a.in).atDepth(0.1), []);
-  t.deepEqual(fn(map.a.in).atDepth(false), []);
-  t.deepEqual(fn(map.a.in).atDepth('x'), []);
-})
+// test.skip('.atDepth() lists types at given depth', t => {
+//   t.deepEqual(fn(map.o.in).atDepth(0), ['number', 'number']);
+//   t.deepEqual(fn(map.o.in).atDepth(2), ['null']);
+//   t.deepEqual(fn(map.o.in).atDepth(4), ['string']);
+//   t.deepEqual(fn(map.o.in).atDepth(5), []);
+//   t.deepEqual(fn(map.o.in).atDepth(1.2), []);
+//   t.deepEqual(fn(map.o.in).atDepth(-2), []);
+//   t.deepEqual(fn(map.o.in).atDepth(+Infinity), []);
+//   t.deepEqual(fn(map.o.in).atDepth(-Infinity), []);
+
+//   t.deepEqual(fn(map.a.in).atDepth(0), ['number', 'undefined']);
+//   t.deepEqual(fn(map.a.in).atDepth(1), ['number', 'string']);
+//   t.deepEqual(fn(map.a.in).atDepth(0.1), []);
+//   t.deepEqual(fn(map.a.in).atDepth(false), []);
+//   t.deepEqual(fn(map.a.in).atDepth('x'), []);
+// })
